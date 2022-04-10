339642a45249374c351012fb930f6424c7b34ccc
Pramod Silwal <silwalpramod99@gmail.com>
2022-04-10 20:46:07 +0545

    first message

---
+++ app/src/main/java/com/example/menu_option/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.menu_option;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=findViewById(R.id.text1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_option,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.setting:
+                text.setText("setting is clicked");
+                break;
+            case R.id.bluetooth:
+                text.setText("Bluetooth is clicked");
+                break;
+            case R.id.music:
+                text.setText("music is selected");
+                break;
+        }
+        return true;
+    }
+}
